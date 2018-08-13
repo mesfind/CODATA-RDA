25bc83ebc91c1256b2649766ef44104cc22b1899
Mesfin Diro <mesfin.diro@aau.edu.et>
2018-08-13 10:37:17 +0200

    update of the file

--- advance.R
+++ advance.R
@@ -1,7 +1,7 @@
 # to install jrGgplot2 package
-install.packages("drat")
-drat::addRepo("jr-packages")
-install.packages("jrGgplot2", dependencies = TRUE)
+# install.packages("drat")
+# drat::addRepo("jr-packages")
+# install.packages("jrGgplot2", dependencies = TRUE)
 
 library(jrGgplot2)
 library(ggplot2)
@@ -53,6 +53,36 @@ g + geom_bar(aes(x= gender, fill=dec),
 g + geom_bar(aes(x=gender, fill=dec), position = "dodge")
 
 
+data(bond, package = "jrGgplot2")
 
+g = ggplot(data = bond,
+           mapping = aes(x = Kills,
+                         y = Alcohol_Units))
+# print the first layer of our plot
+g
 
+# Add layers to our plot
+(g = ggplot(bond, aes(Actor, Alcohol_Units)))
+(g1 =g + geom_boxplot())
+
+g + geom_violin()
+
+g + geom_violin()+
+  geom_boxplot()
+
+g + geom_violin()+
+  geom_boxplot()+
+  geom_point()
+# place a color on the box plot
+# Box plot aesthetic by fill
+(g2 = g + geom_boxplot(aes(fill=Actor)))
+
+# Box plot aesthetic by group
+(g + geom_boxplot(aes(group=Actor)))
+
+# Box plot aesthetic by color
+(g + geom_boxplot(aes(color=Actor)))
+
+# Box plot aesthetic by weight
+(g + geom_boxplot(aes(weight=Kills)))
              
