02b7dba4da2dbf093a36dda31828969bccab209c
Mesfin Diro <mesfin.diro@aau.edu.et>
2018-08-13 10:06:58 +0200

    updated version of the file

--- advance.R
+++ advance.R
@@ -7,3 +7,52 @@ library(jrGgplot2)
 library(ggplot2)
 
 data(Beauty,package = "jrGgplot2")
+
+g <-  ggplot(data = Beauty)
+
+g1 <- g + geom_point(aes(x = age, y = beauty))
+
+
+g + geom_point(aes(x = age, y = beauty, color=gender))
+
+g + geom_point(aes(x = age,
+                   y = beauty,
+                   alpha=evaluation,
+                   color=gender))
+#why the difference ??
+g + geom_point(aes(x = age, y = beauty, color=tenured))
+
+g + geom_point(aes(x = age, y =beauty,
+                   color=factor(tenured)))
+
+# Box plots 
+# update from practical sheet
+
+g + geom_boxplot(aes(x = gender,
+                     y = beauty, 
+                     color=tenured))
+## OK
+g + geom_boxplot(aes(x = gender,
+                     y =beauty,
+                   color=factor(tenured)))
+
+# Bar plot
+g + geom_bar(aes(x=factor(tenured)))
+
+# First, we round ages to the nearest
+# decade:
+  
+Beauty$dec = factor(signif(Beauty$age, 1))
+g = ggplot(data = Beauty)
+
+g + geom_bar(aes(x= gender, fill=dec))
+
+g + geom_bar(aes(x= gender, fill=dec),
+                 position="stack")
+
+g + geom_bar(aes(x=gender, fill=dec), position = "dodge")
+
+
+
+
+             
